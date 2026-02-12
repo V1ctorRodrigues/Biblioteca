fc981c1a56dbf9fe27980738f7eef98e6c9dd850
V1ctorRodrigues <victor.rodrigueslima@ufrpe.br>
2026-02-12 00:21:19 -0300

    Criação de Material

---
+++ src/Material.java
@@ -0,0 +1,33 @@
+public abstract class Material{
+    private String id;
+    private String titulo;
+    private boolean disponivel = true;
+
+    public String getId() {
+        return id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public boolean getDisponivel(){
+        return disponivel;
+    }
+    public Material(String id, String titulo) {
+        this.titulo = titulo;
+        this.id = id;
+    }
+    public boolean emprestar(){
+        if(disponivel){
+            disponivel = false;
+            return true;
+        }
+            return false;
+    }
+    public boolean devolver(){
+        if(!disponivel){
+            disponivel = true;
+            return true;
+        }
+            return false;
+    }
+}
